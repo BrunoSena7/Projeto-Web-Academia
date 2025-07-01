262877156cd6fd13c2358891326a6a7159700476
BrunoSena7 <senabrunomessi@gmail.com>
2025-07-01 14:51:20 -0300

    Reconectando repositório e atualizando imagens e melhorias visuais

---
+++ Back-end/aula0304jeof.py
@@ -0,0 +1,19 @@
+dia_da_semana = int(input("Digite um número de 1 a 7 e tecle enter"))
+
+if dia_da_semana == 1:
+    print ("Você escolheu a segunda-feira")
+elif dia_da_semana == 2:
+    print ("Você escolheu a terça-feira")
+elif dia_da_semana == 3:
+    print ("Você escolheu a quarta-feira")
+elif dia_da_semana == 4:
+    print ("Você escolheu a quinta-feira")
+elif dia_da_semana == 5:
+    print ("Você escolheu a sexta-feira")
+elif dia_da_semana == 6:
+    print ("Você escolheu o sábado")
+elif dia_da_semana == 7:
+    print ("Você escolheu o domingo")
+else:
+    print ("O número digitado não tem um dia correspondente")
+
---
+++ Back-end/main.py
@@ -0,0 +1,17 @@
+
+# Código corrigido #
+nome = input("Digite o seu nome: ")
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+print(nome)
+print(peso)
+print(altura)
+
+if peso >= 110:
+    print("Sobrepeso detectado")
+
+if altura <= 1.85:
+    print("Levemente acima do peso")
+
+
